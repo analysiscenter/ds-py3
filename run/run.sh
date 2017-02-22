e68dceaeaa1dbe11ab67d23db0007a5bb89db19a
Roman Kh <rhudor@gmail.com>
2017-02-22 21:06:40 +0300

    Changed Shippable DockerHub integration to analysiscenter

--- run/run.sh
+++ run/run.sh
@@ -6,7 +6,7 @@ notebooks_vol=${DS_NOTEBOOKS_DIR:-`pwd`/notebooks}
 config_vol=${DS_CONFIG_DIR:-`pwd`/config}
 secret_vol=${DS_SECRET_DIR:-`pwd`/secret}
 port=${DS_PORT:-8888}
-image=${DS_IMAGE:-rhudor/ds-py3}
+image=${DS_IMAGE:-analysiscenter1/ds-py3}
 
 
 sudo docker run -p ${port}:8888 \
