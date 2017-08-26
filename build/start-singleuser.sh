bfef350713b8f24e5b19b654d785c07d266ffafd
Roman Kh <roman-kh@users.noreply.github.com>
2017-08-26 11:09:57 +0300

    Update start-singleuser.sh

--- build/start-singleuser.sh
+++ build/start-singleuser.sh
@@ -4,6 +4,7 @@ exec jupyterhub-singleuser \
   --port=8888 \
   --ip=0.0.0.0 \
   --user=$JPY_USER \
+  --allow-root \
   --cookie-name=$JPY_COOKIE_NAME \
   --base-url=$JPY_BASE_URL \
   --hub-prefix=$JPY_HUB_PREFIX \
