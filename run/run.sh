19ba472d28fff88491adf3f1c15dc4ee35546f62
Roman Kh <rhudor@gmail.com>
2018-03-30 14:23:49 +0300

    Run container with TF 1.6

--- run/run.sh
+++ run/run.sh
@@ -6,7 +6,7 @@ notebooks_vol=${DS_NOTEBOOKS_DIR:-`pwd`/notebooks}
 config_vol=${DS_CONFIG_DIR:-`pwd`/config}
 secret_vol=${DS_SECRET_DIR:-`pwd`/secret}
 port=${DS_PORT:-8888}
-image=${DS_IMAGE:-analysiscenter1/ds-py3:gpu}
+image=${DS_IMAGE:-analysiscenter1/ds-py3:gpu-1.6}
 
 
 sudo docker run --runtime=nvidia --rm -p ${port}:8888 \
