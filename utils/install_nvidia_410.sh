d0607d3ded04d02a4c420331aba633f59d9ac2bf
Dmitry Povyaznikov <d.podvyaznikov@analysiscenter.org>
2019-02-24 18:00:59 +0300

    Update driver version

--- utils/install_nvidia_410.sh
+++ utils/install_nvidia_410.sh
@@ -3,6 +3,6 @@
 sudo apt-get purge nvidia*
 sudo add-apt-repository ppa:graphics-drivers/ppa
 sudo apt update
-sudo apt install -y --no-install-recommends nvidia-390
+sudo apt install -y --no-install-recommends nvidia-410
 
 nvidia-smi
