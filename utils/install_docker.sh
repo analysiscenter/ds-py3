50b4bcbc74b618a6b0117a1fe5729c3ab361003a
Dmitry Podvyaznikov <podviaznikoff@gmail.com>
2019-11-29 23:24:09 +0300

    Reconcile with master

--- utils/install_docker.sh
+++ utils/install_docker.sh
@@ -5,11 +5,11 @@ sudo apt-get install -y apt-transport-https ca-certificates curl software-proper
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-add-apt-repository \
+sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 
 sudo apt-get update
 
-sudo apt-get install -y docker-ce=17.12.0~ce-0~ubuntu
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
