02752cf9993c858f4942c30341bf6edae59899c5
Dmitry Podvyaznikov <podviaznikoff@gmail.com>
2020-10-27 23:43:23 +0300

    Change base image; update to driver to 450

--- utils/install_nvidia_430_18.04.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-sudo apt-get purge nvidia* -y
-sudo add-apt-repository ppa:graphics-drivers/ppa -y
-sudo apt update
-sudo apt install -y --no-install-recommends nvidia-driver-430 nvidia-settings
-
-nvidia-smi
