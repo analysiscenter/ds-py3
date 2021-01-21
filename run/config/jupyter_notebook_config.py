48748dd19354ef3606f008070d8d0c0489a87959
Roman Kh <rhudor@gmail.com>
2021-01-21 13:23:09 +0300

    Use bash terminal

--- run/config/jupyter_notebook_config.py
+++ run/config/jupyter_notebook_config.py
@@ -3,3 +3,4 @@ c.NotebookApp.port = 8888
 c.NotebookApp.open_browser = False
 c.NotebookApp.token = u'password'
 c.NotebookApp.allow_root = True
+c.NotebookApp.terminado_settings = { 'shell_command': ['bash'] }
