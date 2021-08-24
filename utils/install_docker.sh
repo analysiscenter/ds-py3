26797760fa16c56b0572bc14d8357d4d16ee2386
Dmitry Podvyaznikov <podviaznikoff@gmail.com>
2021-08-24 12:03:13 +0300

    Update base image, libs and nvidia-driver

--- utils/install_docker.sh
+++ utils/install_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # install Docker
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
@@ -10,17 +10,17 @@ sudo add-apt-repository \
    $(lsb_release -cs) \
    stable"
 
-sudo apt-get update
+sudo apt update
 
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo apt install -y docker-ce docker-ce-cli containerd.io
 
 # install NVIDIA docker version 2
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
 curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | \
  sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-sudo apt-get update
-sudo apt-get install -y nvidia-container-toolkit
+sudo apt update
+sudo apt install -y nvidia-container-toolkit
 
 # reload the Docker daemon configuration
 sudo systemctl restart docker
---
+++ utils/install_nvidia_470.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+sudo apt purge libnvidia* -y
+sudo apt purge nvidia* -y
+sudo apt dist-upgrade --fix-broken -y
+sudo add-apt-repository ppa:graphics-drivers/ppa -y
+sudo apt update
+sudo apt install -y --no-install-recommends nvidia-driver-470 nvidia-settings
+
+nvidia-smi
