ccb9e0a8fd6725b7d0140f5d240f7c1d7994bc9a
Dmitry Podvyaznikov <podviaznikoff@gmail.com>
2022-03-25 16:18:07 +0300

    Add default pass

--- run/config/jupyter_notebook_config.py
+++ run/config/jupyter_notebook_config.py
@@ -1,6 +1,6 @@
 c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.port = 8888
 c.NotebookApp.open_browser = False
-c.NotebookApp.password = 'password'
+c.NotebookApp.password = 'sha256:f983cdad1097:8d0ba471b97eba20391afd6cf5b9f0acd44ef1dfaf1993159534efb7409c13de'
 c.NotebookApp.allow_root = True
 c.NotebookApp.terminado_settings = { 'shell_command': ['bash'] }
