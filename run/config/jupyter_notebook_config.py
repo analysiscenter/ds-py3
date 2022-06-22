df1f9df9cd0392fdf57ab237ecb2e7c2cd23b0c8
Dmitry Podvyaznikov <podviaznikoff@gmail.com>
2022-06-22 17:54:46 +0300

    Add libs

--- run/config/jupyter_notebook_config.py
+++ run/config/jupyter_notebook_config.py
@@ -1,6 +1,7 @@
 c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.port = 8888
 c.NotebookApp.open_browser = False
-c.NotebookApp.password = 'sha256:f983cdad1097:8d0ba471b97eba20391afd6cf5b9f0acd44ef1dfaf1993159534efb7409c13de'
+# Hash for `password`
+c.NotebookApp.password = 'sha256:bb2339985d35:8d674e9e4ad4a7bec1715dbb94b357c79df2f02d62ce1b90c12136b3dbe8203e'
 c.NotebookApp.allow_root = True
 c.NotebookApp.terminado_settings = { 'shell_command': ['bash'] }
