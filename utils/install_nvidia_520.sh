aa0ba0c44b9eb62690fe8979edfca6850e3f8f1c
Dmitry Podvyaznikov <podviaznikoff@gmail.com>
2022-10-25 10:53:28 +0300

    Update ipympl; remove gimli

--- utils/install_nvidia_520.sh
+++ utils/install_nvidia_520.sh
@@ -5,6 +5,6 @@ sudo apt purge nvidia* -y
 sudo apt dist-upgrade --fix-broken -y
 sudo add-apt-repository ppa:graphics-drivers/ppa -y
 sudo apt update
-sudo apt install -y --no-install-recommends nvidia-driver-515 nvidia-settings
+sudo apt install -y --no-install-recommends nvidia-driver-520 nvidia-settings
 
 nvidia-smi
