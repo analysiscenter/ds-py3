693c6b6d9b65bafc1c7fd174d4878c71ea9aa58c
Dmitry Podvyaznikov <podviaznikoff@gmail.com>
2023-04-24 14:01:15 +0000

    Move to ubuntu lunar base image

--- utils/install_nvidia_525.sh
+++ utils/install_nvidia_525.sh
@@ -6,5 +6,3 @@ sudo apt dist-upgrade --fix-broken -y
 sudo add-apt-repository ppa:graphics-drivers/ppa -y
 sudo apt update
 sudo apt install -y --no-install-recommends nvidia-driver-525 nvidia-settings
-
-nvidia-smi
